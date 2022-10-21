707be09e3be1bee47b1fa809fde6834be3c720a2
Arindam <arindambhowal10@gmail.com>
2022-10-21 15:25:44 +0530

    first commit

---
+++ LMS/backend/models/Student.js
@@ -0,0 +1,24 @@
+
+
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type:String, required: true, unique: true},
+    password: {type: String, required: true},
+    profilePic: String,
+    rollNo : {type: String, required: true, unique: true},
+    courseName : {type: String, required: true},
+    deptName: {type: String, required: true},
+    address: {type: String, required: true},
+    phoneNumber: {type: Number, required: true},
+    semester : {type: Number, required: true},
+    father_name : String,
+    mother_name: String,
+    assignments: Array,
+    examinations : Array,
+    attendence : Array,
+    personal_details: Array
+})
+
+module.exports = mongoose.model('Students', studentSchema)
